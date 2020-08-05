a46d3305cd7098a6caaad65b5d711e44477f0ffa
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2020-08-05 14:10:07 -0300

    Revert "Merge branch 'master' into allhands2020"
    This reverts commit 49f74598ee72b567b8ae39b5ba21aff7c18fbc62, reversing
    changes made to 6ed2e9bc7d9c450a72186dc14ee85b3eed984381.

--- src/templates/marketing-page.js
+++ src/templates/marketing-page.js
@@ -8,7 +8,6 @@ import LobbyHeroMarketing from '../components/LobbyHeroMarketing'
 import ScheduleLiteComponent from "../components/ScheduleLiteComponent"
 import DisqusComponent from '../components/DisqusComponent'
 import ClockComponent from '../components/ClockComponent'
-import Countdown from '../components/Countdown'
 import Content, { HTMLContent } from '../components/Content'
 import envVariables from '../utils/envVariables';
 
@@ -32,7 +31,7 @@ export const MarketingPageTemplate = class extends React.Component {
   }
 
   render() {
-    let { content, contentComponent, user, summit, loggedUser, isLoggedUser, marketingNow, nowUtc } = this.props;
+    let { content, contentComponent, user, summit, loggedUser, isLoggedUser, marketingNow } = this.props;
 
     const PageContent = contentComponent || Content
 
@@ -43,7 +42,6 @@ export const MarketingPageTemplate = class extends React.Component {
         <React.Fragment>
           <ClockComponent summit={summit} />
           <LobbyHeroMarketing summit={summit} />
-          <Countdown now={nowUtc} summit={summit} />
           <div className="columns" id="marketing-columns">
             <div className="column is-half px-6 py-6">
               {MarketingSite.leftColumn.schedule &&
@@ -97,7 +95,7 @@ MarketingPageTemplate.propTypes = {
   isLoggedUser: PropTypes.bool,
 }
 
-const MarketingPage = ({ data, user, summit, marketingNow, nowUtc, loggedUser, isLoggedUser, getSummitData, getTimeNow, getDisqusSSO }) => {
+const MarketingPage = ({ data, user, summit, marketingNow, loggedUser, isLoggedUser, getSummitData, getTimeNow, getDisqusSSO }) => {
   const { frontmatter, html } = data.markdownRemark
 
   return (
@@ -109,7 +107,6 @@ const MarketingPage = ({ data, user, summit, marketingNow, nowUtc, loggedUser, i
         summit={summit}
         loggedUser={loggedUser}
         marketingNow={marketingNow}
-        nowUtc={nowUtc}
         isLoggedUser={isLoggedUser}
         getSummitData={getSummitData}
         getTimeNow={getTimeNow}
@@ -138,7 +135,6 @@ const mapStateToProps = ({ userState, summitState, loggedUserState }) => ({
   isLoggedUser: loggedUserState.isLoggedUser,
   loggedUser: loggedUserState,
   marketingNow: summitState.marketingNow,
-  nowUtc: summitState.nowUtc,
   user: userState,
   summit: summitState.summit,
 })
