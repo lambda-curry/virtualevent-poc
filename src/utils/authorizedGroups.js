5ff5bf6139778b94cd35ab41c0d71d70bf7add7d
Tomás Castillo <tcastilloboireau@gmail.com>
2020-09-17 14:19:43 -0300

    adding validation before split

--- src/utils/authorizedGroups.js
+++ src/utils/authorizedGroups.js
@@ -4,13 +4,13 @@ let authorizedGroups = envVariables.AUTHZ_USER_GROUPS;
     authorizedGroups = authorizedGroups && authorizedGroups !== '' ? authorizedGroups.split(' ') : [];
 
     
-let authorizedSessionPerBadge = envVariables.AUTHZ_SESSION_BADGES;    
-    authorizedSessionPerBadge = authorizedSessionPerBadge.split('|').map((session => {
+let authorizedSessionPerBadge = envVariables.AUTHZ_SESSION_BADGE;    
+    authorizedSessionPerBadge = authorizedSessionPerBadge && authorizedSessionPerBadge !== '' ? authorizedSessionPerBadge.split('|').map((session => {
       let id = session.split(':')[0];      
       let values = session.split(':')[1].split(',');
       let sessionObject = { sessionId: id, authorizedBadges: values };
       return sessionObject
-    }));
+    })) : [];
 
 export const isAuthorizedUser = (groups) => {
   return groups ? groups.some(group => authorizedGroups.includes(group.code)) : false;
