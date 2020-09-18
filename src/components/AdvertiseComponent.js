713a675c4a5a946bd7265a9e7ee44fd230dbda77
Tomás Castillo <tcastilloboireau@gmail.com>
2020-09-18 17:52:19 -0300

    ads visible for certain badges

--- src/components/AdvertiseComponent.js
+++ src/components/AdvertiseComponent.js
@@ -6,42 +6,99 @@ import styles from '../styles/advertise.module.scss'
 
 import Content from '../content/ads.json'
 
-const AdvertiseComponent = ({ section, column, id, className }) => {
+import { getUserBadges } from '../utils/authorizedGroups'
+
+const AdvertiseComponent = ({ section, column, id, badges, className }) => {
+
+  const userBadges = getUserBadges(badges);
 
   const sectionAds = Content.ads.find(ad => ad.section === section).columns.find(c => c.column === column).columnAds;
 
   if (sectionAds.length > 0) {
     return (
       sectionAds.map((ad, index) => {
-        return (
-          ad.id ?
-            ad.id === id ?
-              column === 'center' ?
-                null
-                :
-                <div className={`${styles.sponsorContainer} sponsor-container`} key={index}>
-                  {!ad.button.text && ad.button.link &&
-                    <Link to={ad.button.link}>
-                      <img src={ad.image} alt="sponsor" />
-                    </Link>
-                  }
-                  {ad.button.text && ad.button.link &&
-                    <React.Fragment>
-                      <img src={ad.image} alt="sponsor" />
-                      <Link className={styles.link} to={ad.button.link}>
-                        <button className={`${styles.button} button is-large`}>
-                          <b>{ad.button.text}</b>
-                        </button>
-                      </Link>
-                    </React.Fragment>
-                  }
-                  {!ad.button.text && !ad.button.link &&
-                    <img src={ad.image} alt="sponsor" />
-                  }
-                </div>
-              :
+        console.log(ad.id, id, ad.badgeId)
+        if (ad.id && ad.badgeId && `${ad.id}` === id && userBadges.includes(ad.badgeId)) {
+          console.log(ad.id, ad.badgeId)
+          return (
+            column === 'center' ?
               null
-            :
+              :
+              <div className={`${styles.sponsorContainer} sponsor-container`} key={index}>
+                {!ad.button.text && ad.button.link &&
+                  <Link to={ad.button.link}>
+                    <img src={ad.image} alt="sponsor" />
+                  </Link>
+                }
+                {ad.button.text && ad.button.link &&
+                  <React.Fragment>
+                    <img src={ad.image} alt="sponsor" />
+                    <Link className={styles.link} to={ad.button.link}>
+                      <button className={`${styles.button} button is-large`}>
+                        <b>{ad.button.text}</b>
+                      </button>
+                    </Link>
+                  </React.Fragment>
+                }
+                {!ad.button.text && !ad.button.link &&
+                  <img src={ad.image} alt="sponsor" />
+                }
+              </div>
+          )
+        }
+        if (ad.id && !ad.badgeId && ad.id === id) {
+          return (
+            column === 'center' ?
+              null
+              :
+              <div className={`${styles.sponsorContainer} sponsor-container`} key={index}>
+                {!ad.button.text && ad.button.link &&
+                  <Link to={ad.button.link}>
+                    <img src={ad.image} alt="sponsor" />
+                  </Link>
+                }
+                {ad.button.text && ad.button.link &&
+                  <React.Fragment>
+                    <img src={ad.image} alt="sponsor" />
+                    <Link className={styles.link} to={ad.button.link}>
+                      <button className={`${styles.button} button is-large`}>
+                        <b>{ad.button.text}</b>
+                      </button>
+                    </Link>
+                  </React.Fragment>
+                }
+                {!ad.button.text && !ad.button.link &&
+                  <img src={ad.image} alt="sponsor" />
+                }
+              </div>
+          )
+        }
+        if (ad.badgeId && !ad.id && userBadges.includes(ad.badgeId)) {
+          return (
+            <div className={`${styles.sponsorContainer} sponsor-container`} key={index}>
+              {!ad.button.text && ad.button.link &&
+                <Link to={ad.button.link}>
+                  <img src={ad.image} alt="sponsor" />
+                </Link>
+              }
+              {ad.button.text && ad.button.link &&
+                <React.Fragment>
+                  <img src={ad.image} alt="sponsor" />
+                  <Link className={styles.link} to={ad.button.link}>
+                    <button className={`${styles.button} button is-large`}>
+                      <b>{ad.button.text}</b>
+                    </button>
+                  </Link>
+                </React.Fragment>
+              }
+              {!ad.button.text && !ad.button.link &&
+                <img src={ad.image} alt="sponsor" />
+              }
+            </div>
+          )
+        }
+        if (!ad.if && !ad.badgeId) {
+          return (
             column === 'center' ?
               <div className={`${styles.sponsorContainerCenter} ${className}`} key={index}>
                 <div className={styles.containerText}>
@@ -77,7 +134,8 @@ const AdvertiseComponent = ({ section, column, id, className }) => {
                   <img src={ad.image} alt="sponsor" />
                 }
               </div>
-        )
+          )
+        }
       })
     )
   } else {
