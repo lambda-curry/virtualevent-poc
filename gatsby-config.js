3736644466354cbb483962743294659b1a04b547
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2021-07-05 14:46:36 -0300

    Adds FranklinGothic embedded myfont.

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,14 @@ module.exports = {
       },
     },
     {
+      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static/fonts`,
+        name: 'fonts',
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
