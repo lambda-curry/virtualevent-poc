ef1d269f00b3861b144caed320f6ee5c41256c9e
Tomás Castillo <tcastilloboireau@gmail.com>
2021-08-24 16:55:25 -0300

    Filters fixed and independent scroll

--- src/templates/schedule-page.js
+++ src/templates/schedule-page.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { navigate } from 'gatsby'
 import { connect } from 'react-redux'
@@ -8,6 +8,7 @@ import FullSchedule from '../components/FullSchedule'
 import ScheduleFilters from "../components/ScheduleFilters";
 import AttendanceTrackerComponent from '../components/AttendanceTrackerComponent'
 
+import styles from '../styles/schedule.module.scss'
 
 import { PHASES } from '../utils/phasesUtils'
 
@@ -23,14 +24,29 @@ const SchedulePage = ({ summitPhase, isLoggedUser, location }) => {
     }
   }
 
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <Layout location={location}>
-      <div className="container" style={{position: "static"}}>
+      <div className="container" style={{ position: "static" }}>
         <div className="columns">
           <div className="column is-three-quarters px-6 pt-6 pb-0">
             <FullSchedule {...scheduleProps} />
           </div>
-          <div className="column is-one-quarter px-6 pt-6 pb-0">
+          <div className={`column is-one-quarter px-6 pt-0 pb-6 ${styles.filterContainer}`} style={{top: scrollPosition < 70 ? 70 - scrollPosition : 0 }}>
             <ScheduleFilters />
           </div>
         </div>
