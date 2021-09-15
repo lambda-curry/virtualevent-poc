b1c758e279471ade778fd692a0c9577beadf0891
Tomás Castillo <tcastilloboireau@gmail.com>
2021-09-15 11:38:18 -0300

    fix internal navigation, removes default target

--- src/components/Link.js
+++ src/components/Link.js
@@ -30,7 +30,7 @@ const Link = ({ children, to, activeClassName, partiallyActive, ...other }) => {
     )
   }
   return (
-    <a href={to} {...other} target="_blank" rel="noreferrer">
+    <a href={to} {...other} rel="noreferrer">
       {children}
     </a>
   )
