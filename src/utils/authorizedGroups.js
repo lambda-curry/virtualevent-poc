99735c928ec80e302fd057d59a741ca129bb6983
Santi <santipalenque@gmail.com>
2021-10-15 12:15:47 -0300

    checking badge access level against track access level

--- src/utils/authorizedGroups.js
+++ src/utils/authorizedGroups.js
@@ -1,43 +1,64 @@
-import { getEnvVariable, AUTHZ_USER_GROUPS, AUTHZ_SESSION_BADGE } from './envVariables';
+import {
+  getEnvVariable,
+  AUTHZ_USER_GROUPS,
+  AUTHZ_SESSION_BADGE,
+} from "./envVariables";
 
 export const isAuthorizedUser = (groups) => {
   let authorizedGroups = getEnvVariable(AUTHZ_USER_GROUPS);
-      authorizedGroups = authorizedGroups && authorizedGroups !== '' ? authorizedGroups.split(' ') : [];
-  return groups ? groups.some(group => authorizedGroups.includes(group.code)) : false;
+  authorizedGroups =
+    authorizedGroups && authorizedGroups !== ""
+      ? authorizedGroups.split(" ")
+      : [];
+  return groups
+    ? groups.some((group) => authorizedGroups.includes(group.code))
+    : false;
+};
+
+const getUserBadgeFeatureIds = (summit_tickets) => {
+    return summit_tickets?.reduce((result, item) => {
+        const newFeatureIds = item?.badge?.features?.map(f => f.id).filter(fid => !result.includes(fid)) || [];
+        return [...result, ...newFeatureIds];
+    }, []) || [];
+};
+
+const getUserAccessLevelIds = (summit_tickets) => {
+    return summit_tickets?.reduce((result, item) => {
+        const newAccessLevels = item?.badge?.type?.access_levels?.map(al => al.id).filter(aln => !result.includes(aln)) || [];
+        return [...result, ...newAccessLevels];
+    }, []) || [];
+};
+
+const processAuthSessionBadge = () => {
+    const authSessionBadgeEnv = getEnvVariable(AUTHZ_SESSION_BADGE) || "";
+
+    if (!authSessionBadgeEnv) return [];
+
+    return authSessionBadgeEnv
+        .split("|")
+        .map(item => {
+            const itemArray = item?.split(":");
+            const eventId = parseInt(itemArray[0]);
+            const featuresNeeded = itemArray[1].split(",").map(parseInt);
+            return { eventId, featuresNeeded };
+        });
 }
 
-export const getUserBadges = (summit_tickets) => {
-  let badges = [];
+export const isAuthorizedBadge = (event, summit_tickets) => {
+    let allowed = true;
+    const userFeatures = getUserBadgeFeatureIds(summit_tickets);
+    const userAccessLevels = getUserAccessLevelIds(summit_tickets);
+    const badgeGatedEvents = processAuthSessionBadge(); // TODO move this out of here and run only once
+    const authzSession = badgeGatedEvents.find(ev => ev.eventId === event?.id);
+    const trackAccessLevelIds = event?.track?.allowed_access_levels.map(aal => aal.id) || [];
 
-  if (summit_tickets) {
-      summit_tickets.filter(t => t.badge).forEach(t => {
-          t.badge.features.forEach(feature => {
-              if (!badges.some(e => e === feature.id)) {
-                  badges.push(feature.id);
-              }
-          })
-      });
-  }
+    if (authzSession) {
+        allowed = authzSession.featuresNeeded.some(featId => userFeatures.includes(featId));
+    }
 
-  return badges;
-}
+    if (trackAccessLevelIds.length > 0) {
+        allowed = allowed && trackAccessLevelIds.some(tal => userAccessLevels.includes(tal));
+    }
 
-export const isAuthorizedBadge = (session, summit_tickets) => {
-  let authorizedSessionPerBadge = getEnvVariable(AUTHZ_SESSION_BADGE);
-    authorizedSessionPerBadge = authorizedSessionPerBadge && authorizedSessionPerBadge !== '' ? authorizedSessionPerBadge.split('|').map((session => {
-      const id = session.split(':')[0];
-      const values = session.split(':')[1].split(',');
-      return { sessionId: id, authorizedBadges: values };
-    })) : [];
-
-  let badges = getUserBadges(summit_tickets);
-
-  const authzSession = authorizedSessionPerBadge.find(s => s.sessionId === session);
-  if (authzSession) {
-      return authzSession.authorizedBadges.some(b => {
-          return badges.includes(parseInt(b))
-      });
-  }
-  // we are allowed to enter
-  return true;
+    return allowed;
 };
