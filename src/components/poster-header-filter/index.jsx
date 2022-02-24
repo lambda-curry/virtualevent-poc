5c27298ab964de64f585e8b7f339d86bc3c5b2f1
Tomás Castillo <tcastilloboireau@gmail.com>
2022-02-24 18:19:52 -0300

    Fix issue with state values
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/poster-header-filter/index.jsx
+++ src/components/poster-header-filter/index.jsx
@@ -1,55 +1,68 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './index.module.scss';
 
 const PosterHeaderFilter = ({ changeHeaderFilter }) => {
 
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [dropdownOption, setDropdownOption] = useState({});
+    const [myVotes, setMyVotes] = useState(false);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [dropdownOption, setDropdownOption] = useState({ value: 'page_random', label: 'Random' },);
 
-  const selectOption = (option) => {
-    setDropdownOption(option);
-    setDropdownOpen(false);
-    changeHeaderFilter(dropdownOption.value)
-  }
+    useEffect(() => {
+        changeHeaderFilter(dropdownOption.value)
+    }, [dropdownOption])
 
-  const dropdownOptions = [
-    { value: 'page_random', label: 'Random' },
-    { value: 'custom_order', label: 'Poster Number' },
-  ]
+    useEffect(() => {
+        changeHeaderFilter(myVotes ? 'my_votes' : null)
+    }, [myVotes])
 
-  return (
-    <div className={styles.posterHeaderFilter}>
-      <button onClick={() => changeHeaderFilter('my_votes')}>
-        <i className='fa fa-heart-o'/>
-        My votes
-      </button>
-      {/* <button>
+    const selectOption = (option) => {
+        setDropdownOption(option);
+        setDropdownOpen(false);
+    }
+
+    const myVotesChange = () => {
+        setMyVotes(!myVotes);
+    }
+
+    const dropdownOptions = [
+        { value: 'page_random', label: 'Random' },
+        { value: 'custom_order_asc', icon: 'fa-arrow-up', label: 'Poster Number' },
+        { value: 'custom_order_desc', icon: 'fa-arrow-down', label: 'Poster Number' },
+    ]
+
+    return (
+        <div className={styles.posterHeaderFilter}>
+            <button onClick={() => myVotesChange()}>
+                <i className={`fa ${myVotes ? 'fa-heart' : 'fa-heart-o'}`} />
+                My votes
+            </button>
+            {/* <button>
         <i className='fa fa-share' />
         Share
       </button> */}
-      <div className={styles.dropdown}>
+            <div className={styles.dropdown}>
         <span onClick={() => setDropdownOpen(!dropdownOpen)}>
-          Order by {dropdownOption.label}
-          <i className='fa fa-caret-down' />
+          Order by {dropdownOption.label} {dropdownOption.icon ? <i style={{ marginLeft: 5 }} className={`fa ${dropdownOption.icon}`} /> : null}
+            <i className='fa fa-caret-down' />
         </span>
-        {dropdownOpen &&
-          <div className={styles.dropdownOptions}>
-            {dropdownOptions.map((e, index) => {
-              return (
-                <span onClick={() => selectOption(e)} key={index}>{e.label}</span>
-              )
-            })}
-          </div>
-        }
-      </div>
-    </div>
-  );
+                {dropdownOpen &&
+                <div className={styles.dropdownOptions}>
+                    {dropdownOptions.map((e, index) => {
+                        return (
+                            <span onClick={() => selectOption(e)} key={index}>{e.label} {e.icon && <i className={`fa ${e.icon}`} />}</span>
+                        )
+                    })}
+                </div>
+                }
+            </div>
+        </div>
+    );
 };
 
 PosterHeaderFilter.propTypes = {
-  changeHeaderFilter: PropTypes.func.isRequired
+    changeHeaderFilter: PropTypes.func.isRequired
 };
 
 export default PosterHeaderFilter;
