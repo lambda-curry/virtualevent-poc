3207cc92104696aa15d85dbf1b163d4e2f8e842c
sebastian marcet <smarcet@gmail.com>
2022-03-14 15:15:38 -0300

    Update user-actions.js
    update disqus slug

--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -59,7 +59,7 @@ export const getDisqusSSO = () => async (dispatch) => {
   return getRequest(
     null,
     createAction(GET_DISQUS_SSO),
-    `${window.IDP_BASE_URL}/api/v1/sso/disqus/fnvirtual-poc/profile?access_token=${accessToken}`,
+    `${window.IDP_BASE_URL}/api/v1/sso/disqus/owg2022/profile?access_token=${accessToken}`,
     customErrorHandler
   )({})(dispatch).then(() => {
   }).catch(e => {
